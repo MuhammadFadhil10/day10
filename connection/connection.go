d12204682e8fd7fc0459036c1f375452f7da34b7
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-25 12:27:21 +0700

    initial commit

---
+++ connection/connection.go
@@ -0,0 +1,28 @@
+package connection
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v4"
+)
+
+
+var Conn *pgx.Conn
+var err error
+
+func DatabaseConnect() {
+	postgreUrl := "postgres://postgres:1010@localhost:5432/db_myproject";
+
+	Conn, err = pgx.Connect(context.Background(), postgreUrl)
+
+	if err != nil {
+		fmt.Println("error: database connection failed")
+		return
+	}
+
+	fmt.Println("Database connected!")
+
+
+
+}
